471bdd95f1f4318719eeb958caab7a3e912f3d2e
nam2000808 <129300795+nam2000808@users.noreply.github.com>
2023-05-23 16:00:50 +0700

    commit message

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,10 +3,220 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "jquery/dist/jquery.min.js";
 import "popper.js/dist/umd/popper.min.js";
 import "bootstrap/dist/js/bootstrap.min.js";
-import "../styles/Slider.css";
+import "../styles/Categories.css";
+
+import categories1 from "../assets/categories/categories (1).png";
+import categories2 from "../assets/categories/categories (2).png";
+import categories3 from "../assets/categories/categories (3).png";
+import categories4 from "../assets/categories/categories (4).png";
+import categories5 from "../assets/categories/categories (5).png";
+import categories6 from "../assets/categories/categories (6).png";
+import categories7 from "../assets/categories/categories (7).png";
+import categories8 from "../assets/categories/categories (8).png";
+import categories9 from "../assets/categories/categories (9).png";
+import categories10 from "../assets/categories/categories (10).png";
+import categories11 from "../assets/categories/categories (11).png";
+import categories12 from "../assets/categories/categories (12).png";
+import categories13 from "../assets/categories/categories (13).png";
+import categories14 from "../assets/categories/categories (14).png";
+import categories15 from "../assets/categories/categories (15).png";
+import categories16 from "../assets/categories/categories (16).png";
+import categories17 from "../assets/categories/categories (17).png";
+import categories18 from "../assets/categories/categories (18).png";
+import categories19 from "../assets/categories/categories (19).png";
+import categories20 from "../assets/categories/categories (20).png";
+import categories21 from "../assets/categories/categories (21).png";
+import categories22 from "../assets/categories/categories (22).png";
+import categories23 from "../assets/categories/categories (23).png";
+import categories24 from "../assets/categories/categories (24).png";
+import categories25 from "../assets/categories/categories (25).png";
 
 const Categories = () => {
-  return <div>Categories</div>;
+  return (
+    <div class="box">
+      <div class="row mx-0 mt-1 categories">
+        <h5 class="p-4">DANH MỤC</h5>
+        <div class="d-flex categories-container">
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories1} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories2} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories3} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories4} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories5} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories6} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories7} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories8} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories9} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories10} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories11} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories12} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories13} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories14} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories15} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories16} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories17} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories18} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories19} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories20} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories21} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories22} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories23} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories24} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+          <div class="d-flex flex-column align-items-center card-categories">
+            <div class="wrap-img">
+              <img class="w-100 h-100" src={categories25} alt="" />
+            </div>
+            <p class="mb-0 text-center mt-2">Makanan &</p>
+            <p class="mb-0 text-center">Minuman</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Categories;
